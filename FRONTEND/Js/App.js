93a01a3d69d97609b5dec695801c6498c14e8c22
joticius <jjgarciaf@uniboyaca.edu.co>
2026-05-22 23:45:24 -0500

    Arreglado exportación de informe a pdf

--- FRONTEND/Js/App.js
+++ FRONTEND/Js/App.js
@@ -5,6 +5,7 @@
 
 const API_BASE = 'http://localhost:9000';
 let sprintData = [];
+let historiasData = [];
 
 function activarTab(tabId) {
     const tabs = document.querySelectorAll('.tab');
@@ -138,7 +139,7 @@ function renderizarHistorias(historias) {
         actualizarResumenHistorias([]);
         return;
     }
-
+    historiasData = historias;
     historias.forEach(function (historia) {
         const badgeClass = calcularBadgeClass(historia.estado);
         const storyRow = document.createElement('div');
@@ -177,6 +178,118 @@ function cargarHistorias() {
         .catch(function (err) {
             console.error('Error cargando historias:', err);
         });
+}
+
+function obtenerNombreSprint(sprintId) {
+    const sprint = sprintData.find(function (item) {
+        return item.id === sprintId;
+    });
+    return sprint ? sprint.nombre : 'Sprint ' + sprintId;
+}
+
+function pdfEscape(text) {
+    return String(text)
+        .replace(/\\/g, '\\\\')
+        .replace(/\(/g, '\\(')
+        .replace(/\)/g, '\\)')
+        .replace(/\r/g, ' ')
+        .replace(/\n/g, ' ');
+}
+
+function descargarPDF(nombreArchivo, contenido) {
+    const blob = new Blob([contenido], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+    const enlace = document.createElement('a');
+    enlace.href = url;
+    enlace.download = nombreArchivo;
+    document.body.appendChild(enlace);
+    enlace.click();
+    document.body.removeChild(enlace);
+    URL.revokeObjectURL(url);
+}
+
+function construirPDF(titulo, lineas) {
+    const bodyLines = [];
+    bodyLines.push('BT');
+    bodyLines.push('/F1 12 Tf');
+    bodyLines.push('50 760 Td');
+    bodyLines.push('(' + pdfEscape(titulo) + ') Tj');
+    bodyLines.push('0 -20 Td');
+
+    lineas.forEach(function (linea, index) {
+        if (index > 0) {
+            bodyLines.push('0 -16 Td');
+        }
+        bodyLines.push('(' + pdfEscape(linea) + ') Tj');
+    });
+
+    bodyLines.push('ET');
+    const body = bodyLines.join('\n') + '\n';
+    const streamLength = new TextEncoder().encode(body).length;
+
+    const header = '%PDF-1.3\n';
+    const obj1 = '1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n';
+    const obj2 = '2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n';
+    const obj3 = '3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Resources << /Font << /F1 5 0 R >> >> /Contents 4 0 R >>\nendobj\n';
+    const obj4 = '4 0 obj\n<< /Length ' + streamLength + ' >>\nstream\n' + body + 'endstream\nendobj\n';
+    const obj5 = '5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\n';
+
+    let pdf = header;
+    const encoder = new TextEncoder();
+    const offsets = [0];
+    let cursor = encoder.encode(pdf).length;
+
+    [obj1, obj2, obj3, obj4, obj5].forEach(function (obj) {
+        offsets.push(cursor);
+        pdf += obj;
+        cursor += encoder.encode(obj).length;
+    });
+
+    const xrefStart = cursor;
+    let xref = 'xref\n0 6\n0000000000 65535 f \n';
+    for (let i = 1; i <= 5; i++) {
+        xref += String(offsets[i]).padStart(10, '0') + ' 00000 n \n';
+    }
+
+    const trailer = 'trailer\n<< /Size 6 /Root 1 0 R >>\nstartxref\n' + xrefStart + '\n%%EOF';
+    return pdf + xref + trailer;
+}
+
+function exportarInforme() {
+    if (!historiasData || historiasData.length === 0) {
+        alert('No hay datos de historias para exportar.');
+        return;
+    }
+
+    const total = historiasData.length;
+    const finalizadas = historiasData.filter(function (h) {
+        return h.estado === 'finalizada';
+    }).length;
+    const bloqueadas = historiasData.filter(function (h) {
+        return h.estado === 'impedimento';
+    }).length;
+    const puntosCompletados = historiasData.filter(function (h) {
+        return h.estado === 'finalizada';
+    }).reduce(function (sum, h) {
+        return sum + Number(h.puntos);
+    }, 0);
+
+    const lineas = [
+        'Total historias: ' + total,
+        'Historias finalizadas: ' + finalizadas,
+        'Historias en impedimento: ' + bloqueadas,
+        'Puntos completados: ' + puntosCompletados,
+        '',
+        'Listado de historias:'
+    ];
+
+    historiasData.forEach(function (historia) {
+        const texto = historia.titulo + ' | ' + historia.responsable + ' | ' + historia.puntos + ' pts | ' + historia.estado + ' | ' + obtenerNombreSprint(historia.sprint_id);
+        lineas.push(texto);
+    });
+
+    const pdf = construirPDF('Informe Sprint CamelloHub', lineas);
+    descargarPDF('informe-sprint.pdf', pdf);
 }
 
 function guardarHistoria() {
@@ -264,6 +377,7 @@ function initApp() {
     document.getElementById('btnInforme').addEventListener('click', function () {
         activarTab('informe');
     });
+    document.getElementById('btnExportarInforme').addEventListener('click', exportarInforme);
 
     cargarSprints();
     cargarHistorias();
